67566b3e0bd46b6702ce498dd71f265115f58e45
LML-Pushpa <pushpakkolhe9@gmail.com>
2025-08-14 07:21:43 +0530

    Queue algorithm

--- Data_Structure_And_Algorithm/src/static_queue_data_structure/Queue.java
+++ Data_Structure_And_Algorithm/src/static_queue_data_structure/Queue.java
@@ -1,5 +1,68 @@
 package static_queue_data_structure;
 
 public class Queue {
+    public int[] queue;
+    public int front;
+    public int rear;
+    public int size;
 
+    // Create a Queue structure
+    public Queue(int size) {
+        front = -1;
+        rear = -1;
+        this.size = size;
+        queue = new int[size];
+    }
+
+    // Operation : Enqueue
+    public void Enqueue(int data) {
+        if (!isFull()) {
+            if (front == -1) { // first element
+                front = 0;
+            }
+            rear++;
+            queue[rear] = data;
+            System.out.println("Enqueued: " + data);
+        } else {
+            System.out.println("Queue overflow...........!!!!!!!!");
+        }
+    }
+
+    // Operation : Dequeue
+    public void Dequeue() {
+        if (!isEmpty()) {
+            int temp = queue[front];
+            System.out.println("Dequeued: " + temp);
+            front++;
+            if (front > rear) { // reset when queue becomes empty
+                front = -1;
+                rear = -1;
+            }
+        } else {
+            System.out.println("Queue underflow.............!!!!!!!");
+        }
+    }
+
+    // Check Queue is Full
+    public boolean isFull() {
+        return rear == size - 1;
+    }
+
+    // Check Queue is Empty
+    public boolean isEmpty() {
+        return (front == -1 || front > rear);
+    }
+
+    // Display
+    public void display() {
+        if (!isEmpty()) {
+            System.out.print("Queue elements: ");
+            for (int i = front; i <= rear; i++) {
+                System.out.print("|" + queue[i] + "|");
+            }
+            System.out.println();
+        } else {
+            System.out.println("Queue is Empty......!!!!!!!!!!!!!");
+        }
+    }
 }
--- Data_Structure_And_Algorithm/src/static_queue_data_structure/QueueClient.java
+++ Data_Structure_And_Algorithm/src/static_queue_data_structure/QueueClient.java
@@ -1,10 +1,67 @@
 package static_queue_data_structure;
 
+import java.util.Scanner;
+
 public class QueueClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        Queue queue = null;
+        int choice;
 
+        while (true) {
+            System.out.println("\n====== Queue Menu ======");
+            System.out.println("1 : Create Queue");
+            System.out.println("2 : Enqueue");
+            System.out.println("3 : Dequeue");
+            System.out.println("4 : Display");
+            System.out.println("0 : Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter size of Queue: ");
+                    int size = sc.nextInt();
+                    queue = new Queue(size);
+                    System.out.println("Queue created with size " + size);
+                    break;
+
+                case 2:
+                    if (queue == null) {
+                        System.out.println("Please create a queue first!");
+                    } else {
+                        System.out.print("Enter element to enqueue: ");
+                        int value = sc.nextInt();
+                        queue.Enqueue(value);
+                    }
+                    break;
+
+                case 3:
+                    if (queue == null) {
+                        System.out.println("Please create a queue first!");
+                    } else {
+                        queue.Dequeue();
+                    }
+                    break;
+
+                case 4:
+                    if (queue == null) {
+                        System.out.println("Please create a queue first!");
+                    } else {
+                        queue.display();
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Exiting...");
+                    sc.close();
+                    return;
+
+                default:
+                    System.out.println("Invalid choice! Please try again.");
+            }
+        }
 	}
 
 }
